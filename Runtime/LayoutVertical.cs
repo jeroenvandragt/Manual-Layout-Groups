0b5b49d0e6066e40ffe7af0221f38b3020381542
jeroenvandragt <jeroen.van.dragt@gmail.com>
2021-12-22 12:46:08 +0100

    Update LayoutVertical to the correct component menu

--- Runtime/LayoutVertical.cs
+++ Runtime/LayoutVertical.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 namespace EasySee.UI
 {
-    [AddComponentMenu("UI/EasySee/ManualVerticalLayout")]
+    [AddComponentMenu("Layout/EasySee/ManualVerticalLayout")]
     public class LayoutVertical : MonoBehaviour
     {
         [SerializeField]
